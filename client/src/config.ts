d83ad91589e028379589c5a15351f94503029ff0
victory osaigbovo <victoryosaigbovo@outlook.com>
2022-07-22 09:50:07 +0100

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'bn4aqwi094';
+export const apiEndpoint = `https://${apiId}.execute-api.us-west-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-ernm7e1r.us.auth0.com',
+  clientId: 'A42Qx6ZMvjh7J1rmRdKFbiN9F0C81eAu',
+  callbackUrl: 'http://localhost:3000/callback'
+};
